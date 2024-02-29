05eae8cb63bb4a76433496993603be8308fe1b68
Riz <riz4re2@gmail.com>
2024-02-29 14:47:09 +1100

    Updated for active states

--- src/components/BarChartComponent.tsx
+++ src/components/BarChartComponent.tsx
@@ -1,7 +1,8 @@
 // Import React, useState, useEffect from React, and necessary components from Recharts
-import React from 'react';
+import React, { useState } from 'react';
 import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer, Cell } from 'recharts';
-import { BarChartProps } from '../types';
+import { SpendingDataType, BarChartProps } from '../types';
+
 
 
 // Function to determine the current day to highlight the corresponding bar
@@ -10,8 +11,36 @@ const getCurrentDay = () => {
     return new Date().toLocaleDateString('en-us', { weekday: 'short' }).toLowerCase();
 };
 
+interface CustomObject {
+    active: boolean,
+    payload: any
+}
+
+const CustomTooltip: React.FC<CustomObject> = ({ active, payload }) => {
+    // Check if a bar is hovered
+    if (!active || !payload) return null;
+ 
+    // Access desired data from payload
+    const data1 = payload[0].payload.amount; // Access data1 from your data object   
+
+    return (
+        <div className="px-2 py-1 bg-black text-white rounded-lg">
+            <p>${data1}</p>          
+        </div>
+    );
+};
+
 // Define the BarChartComponent functional component with its props
 const BarChartComponent: React.FC<BarChartProps> = ({ data }) => {
+    const [hoveredIndex, setHoveredIndex] = useState('');
+
+    const handleMouseEnter = (index: string) => {
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex('');
+    };
 
     const currentDay = getCurrentDay();
 
@@ -22,9 +51,12 @@ const BarChartComponent: React.FC<BarChartProps> = ({ data }) => {
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data}>
                 <XAxis tickLine={false} dataKey="day" />
-                <Tooltip cursor={{ fill: 'transparent' }} />
+                <Tooltip cursor={{ fill: 'transparent' }} content={<CustomTooltip active={hoveredIndex !== null} payload={data} />} />
                 {/* Map over your data to create individual bars */}
-                <Bar dataKey="amount" radius={[5, 5, 0, 0]} barSize={40}>
+                <Bar dataKey="amount" radius={[5, 5, 0, 0]} barSize={40} style={{ cursor: 'pointer' }}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
                     {
                         data.map((entry, index) => {
                             const color = entry.day === currentDay ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)';
@@ -32,9 +64,9 @@ const BarChartComponent: React.FC<BarChartProps> = ({ data }) => {
                         })
                     }
                 </Bar>
-             
+
             </BarChart>
-           
+
         </ResponsiveContainer>
 
     );
